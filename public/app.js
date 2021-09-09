44663a2da161a28099cb67fb31401e3777617cd0
PijushRoy <pijushroy0198@gmail.com>
2021-09-09 12:00:00 +0600

    Update file index, style, app

--- public/app.js
+++ public/app.js
@@ -25,7 +25,9 @@ window.onload = function () {
 
 
     axios.get('/api/history')
-        .then(({ data }) => {
+        .then(({
+            data
+        }) => {
             if (data.length > 0) {
                 updateHistory(data)
             } else {
@@ -44,7 +46,9 @@ window.onload = function () {
                 getWeatherData(e.target.value, null, weather => {
                     e.target.value = ''
                     axios.post('/api/history', weather)
-                        .then(({ data }) => updateHistory(data))
+                        .then(({
+                            data
+                        }) => updateHistory(data))
                         .catch(e => {
                             console.log(e)
                             alert('Error Occurred')
@@ -79,7 +83,7 @@ function getWeatherData(city = DEFAULT_CITY, coords, cb) {
             }
 
             setWeather(weather)
-            if(cb) cb(weather)
+            if (cb) cb(weather)
         })
         .catch(e => {
             console.log(e)
@@ -93,9 +97,9 @@ function setWeather(weather) {
     country.innerHTML = weather.country;
     mainText.innerHTML = weather.main;
     description.innerHTML = weather.description;
-    temp.innerHTML = weather.temp;
-    pressure.innerHTML = weather.pressure;
-    humidity.innerHTML = weather.humidity;
+    temp.innerHTML = Math.round((((weather.temp - 273.15) * 1.8) + 32 - 32) / 1.8) + ' C';
+    pressure.innerHTML = weather.pressure / 1000 + 'kpa';
+    humidity.innerHTML = weather.humidity + '%';
 }
 
 function updateHistory(history) {
@@ -110,9 +114,9 @@ function updateHistory(history) {
         tempHistory.getElementsByClassName('country')[0].innerHTML = h.country
         tempHistory.getElementsByClassName('main')[0].innerHTML = h.main
         tempHistory.getElementsByClassName('description')[0].innerHTML = h.description
-        tempHistory.getElementsByClassName('temp')[0].innerHTML = h.temp
-        tempHistory.getElementsByClassName('pressure')[0].innerHTML = h.pressure
-        tempHistory.getElementsByClassName('humidity')[0].innerHTML = h.humidity
+        tempHistory.getElementsByClassName('temp')[0].innerHTML = Math.round((((h.temp - 273.15) * 1.8) + 32 - 32) / 1.8) + ' C'
+        tempHistory.getElementsByClassName('pressure')[0].innerHTML = h.pressure / 1000 + 'kpa'
+        tempHistory.getElementsByClassName('humidity')[0].innerHTML = h.humidity + '%'
 
         historyElm.appendChild(tempHistory)
     })
